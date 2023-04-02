0387010c0ac2c75db5f5e4980c9031d979698549
james <117844938+leejamesss@users.noreply.github.com>
2023-04-02 13:34:07 +0800

    Update Zipper.py

--- hw/hw04/Zipper/Zipper.py
+++ hw/hw04/Zipper/Zipper.py
@@ -1,70 +1,31 @@
+n = int(input())
 
-#利用动归思想储存算好的结果
-#dp[i][j]表示a的前i个字符和b的前j个字符能否组成c的前i+j个字符
-
-
-# #输出
-# n=int(input())
-# for i in range(n):
-#     a,b,c=input().split()
-#     dp=[[False for i in range(201)]for j in range(201)]
-# #初始化
-#     dp[0][0]=True
-#     for i in range(1,len(a)+1):
-#         if a[i-1]==c[i-1] and dp[i-1][0]:
-#             dp[i][0]=True
-#     for i in range(1,len(b)+1):
-#         if b[i-1]==c[i-1] and dp[0][i-1]:
-#             dp[0][i]=True
-# #动归
-# #i表示a的前i个字符，j表示b的前j个字符
-# #遍历a和b的所有字符
-
-#     for i in range(1,len(a)+1):
-#         for j in range(1,len(b)+1):
-#             if (dp[i-1][j] and a[i-1]==c[i+j-1]) or (dp[i][j-1] and b[j-1]==c[i+j-1]):
-#                 dp[i][j]=True
-#             else:
-#                 dp[i][j]=False
-#     if dp[len(a)][len(b)]:
-#         print(f"Data set {i+1}: yes")
-#     else:
-#         print(f"Data set {i+1}: no")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def can_form_string(a: str, b: str, c: str) -> bool:
+    dp = [True] + [False] * len(b)
     
+    for i in range(len(a) + 1):
+        for j in range(len(b) + 1):
+            if i == 0 and j == 0:
+                continue
+            elif i == 0:
+                dp[j] = dp[j-1] and b[j-1] == c[i+j-1]
+            elif j == 0:
+                dp[j] = dp[j] and a[i-1] == c[i+j-1]
+            else:
+                if a[i-1] == c[i+j-1] and b[j-1] == c[i+j-1]:
+                    dp[j] = dp[j] or dp[j-1]
+                elif a[i-1] == c[i+j-1]:
+                    dp[j] = dp[j]
+                elif b[j-1] == c[i+j-1]:
+                    dp[j] = dp[j-1]
+                else:
+                    dp[j] = False
+    
+    return dp[-1]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(n):
+    str1, str2, Strall = input().split()
+    if can_form_string(str1, str2, Strall):
+        print(f"Data set {i+1}: yes")
+    else:
+        print(f"Data set {i+1}: no")
