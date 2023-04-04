e32d9b99fd2ee8dd3fad2da3ddba25a1cff8b887
james <117844938+leejamesss@users.noreply.github.com>
2023-04-04 16:00:17 +0800

    Update Forest.py

--- class/Forest.py
+++ class/Forest.py
@@ -4,15 +4,15 @@ def woodToBinaryTree(woods):
   biTree=woods[0]                                  #森林的根就是第一棵树的根
   p=biTree                                         #指针，初始位置为森林的根
   for i in range(1,len(woods)):                    
-    p.addRight(woods[i])                           #指针增加这个
+    p.addRight(woods[i])                           #指针增加这个数目
     p=p.right
   return biTree
 
 #二叉树转森林
 def binaryTreeToWoods(tree):
-  p=tree
-  q=p.right
-  p.right=None
+  p=tree                                          #tree指的是二叉树
+  q=p.right                                       #q是p的右节点
+  p.right=None                                    
   woods=[p]
   if q:
     woods+=binaryTreeToWoods(q)
