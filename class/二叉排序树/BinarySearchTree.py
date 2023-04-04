aeb3f5583cc3d8b07ac42f4e08218a69c7fd9439
james <117844938+leejamesss@users.noreply.github.com>
2023-04-04 17:57:01 +0800

    Update BinarySearchTree.py

--- class/二叉排序树/BinarySearchTree.py
+++ class/二叉排序树/BinarySearchTree.py
@@ -47,7 +47,122 @@ class  BinarySearchTree:
         else:
           return insert(root.left）
       elif self.less(root.key,key):
-        if 
+        if root.right is None:
+           root.right = BinarySearchTree.Node(key,data)
+           return True
+        else:
+          return insert(root.right)
+      else:
+        root.value = data
+        return False
+ def _findMin(self,root,father):
+   if root.right is None:
+     return root,father
+   else:
+     return self._findMax(root.right,father)
+ def pop(self,key):
+   nd,father=self._find(key)
+   if nd is None:
+     raise Exception("Key not found")
+   else:
+     self.size-=1
+     self._deleteNode(nd,father)
+     return nd.value
+ def _deleteNode(self,nd,father):
+   if nd.left and nd.right:
+     minNd,father=self._findMin(nd.right,nd)
+     nd.key,nd.value= minNd.key,minNd.value
+     self._deleteNode(minNd,father)
+   elif nd.left:
+     if father and father.left is nd:
+       father.left=nd.left
+     elif father and father.right is nd:
+       father.right=nd.left
+     else:
+       self.root=nd.left
+   elif nd.right:
+     if father and father.right is nd:
+       father.right = nd.right 
+     elif father and father.left is nd:
+       father.left = nd.right 
+     else:
+       self.root=nd.right
+   else:
+     if father and father.left is nd:
+       father.left=None
+     elif father and father.right is nd:
+       father.right=None
+     else:
+       self.root=None 
+def _inorderTraversal(self):
+  def inorderTraversal(root):
+    if root.left:
+      yield from inorderTraversal(root.left)
+    yield root.key,root.value
+    if root.right:
+      yield from inorderTraversal(root.right)
+    if self.root is None：
+      return 
+    yield from inorderTraversal(self.root)
+def __contains__(self,key):
+  return self._find(key)[0] is not None
+def __iter__(self):
+  return self._inorderTraversal()
+def __getitem__(self,key):
+  nd,father=self._find(key)
+  if nd is None:
+    raise Exception("key not found")
+  else:
+    return nd.value
+def __setitem__(self,key,data):
+  nd,father=self._find(key)
+  if nd is None:
+    self._insert(key,data)
+  else:
+    nd.value=data
+def __len__(self)：
+  return self.size
+ 
+                        
+##############################################                       
+                        
+                        
+import random
+random.seed(2)
+s = [i for i in range(8)]
+tree = BinarySearchTree()
+#若 tree = Tree(lambda x ,y : y <x) 则从大到小排
+random.shuffle(s)
+for x in s:
+tree[x] = x #加入关键字为x，值为x的元素
+print(len(tree)) #>>8
+for x in tree: #首先会调用tree.__iter__()返回一个迭代器
+print(f"({x[0]},{x[1]})",end = "") #从小到大遍历整个树
+#>>(0,0)(1,1)(2,2)(3,3)(4,4)(5,5)(6,6)(7,7)
+print()
+print(3000 in tree) #>>False
+print(3 in tree) #>>True
+print(tree[3])                        
+                        
+try:
+  print(tree[3000]) #关键字为3000的元素不存在，此句引发异常
+except Exception as e:
+  print(e) #>>key not found
+tree[3000] = "ok" #添加关键字为3000，值为"ok"的元素
+print(tree[3000],len(tree)) #>>ok 9
+tree[3000] = "bad" #将关键字为3000的元素的值改为"bad"
+print(tree[3000],len(tree)) #>>bad 9
+try:
+  tree.pop(354) #关键字为354的元素不存在，此句引发异常
+except Exception as e:
+  print(e) #>>key not found
+tree.pop(3)
+print(len(tree))                        
+                        
+                      
+                 
+                     
+       
           
           
           
