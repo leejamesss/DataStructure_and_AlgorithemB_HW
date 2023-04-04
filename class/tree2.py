faeb044144cad86aad7cf58958e93446e3487e68
james <117844938+leejamesss@users.noreply.github.com>
2023-04-04 15:35:05 +0800

    Create tree2.py

---
+++ class/tree2.py
@@ -0,0 +1,30 @@
+
+#儿子兄弟表示法（利用二叉树表示树）（把原本的兄弟变为父子）
+#没有右节点
+#使用范围：没有可变长的数列的时候
+
+
+#树的直观表示法转儿子兄弟树
+def treeToBinaryTree(tree):
+  bTree=Binary(tree,data)
+  for i in range(len(tree.subtrees)):
+    if i ==0:
+      tmpTree=treeToBinaryTree(tree.subtrees[i])
+      bTree.addLeft(tmpTree)
+    else:
+      tmpTree.addRight(treeToBinaryTree(tree.subtrees[i]))
+      tmpTree= tmpTree.right
+  return btree
+
+
+#树的儿子兄弟树表示法转换为直观表示法
+
+def binaryTreeToTree(biTree):
+  tree=Tree(biTree.data)
+  son=biTree.left
+  if son:
+    tree.addSubTree(binaryTreeToTree(son))
+    while son.right:
+      tree.addSubTree(binaryTreeToTree(son.right))
+      son=son.right
+  return tree
