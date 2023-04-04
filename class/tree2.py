b449542371d630a71b5d7cc8d2819204c045296d
james <117844938+leejamesss@users.noreply.github.com>
2023-04-04 15:46:16 +0800

    Update tree2.py

--- class/tree2.py
+++ class/tree2.py
@@ -6,14 +6,14 @@
 
 #树的直观表示法转儿子兄弟树
 def treeToBinaryTree(tree):
-  bTree=Binary(tree,data)
-  for i in range(len(tree.subtrees)):
+  bTree=Binary(tree.data)                                 #两个的树根是一样的                  
+  for i in range(len(tree.subtrees)):                     
     if i ==0:
-      tmpTree=treeToBinaryTree(tree.subtrees[i])
-      bTree.addLeft(tmpTree)
-    else:
-      tmpTree.addRight(treeToBinaryTree(tree.subtrees[i]))
-      tmpTree= tmpTree.right
+      tmpTree=treeToBinaryTree(tree.subtrees[i])                    #长子节点转换为树
+      bTree.addLeft(tmpTree)                                        #添加到左边（比较年长）
+    else:                                                 
+      tmpTree.addRight(treeToBinaryTree(tree.subtrees[i]))          #如果见到了其他儿子，加到右边，因为辈分排在后
+      tmpTree= tmpTree.right    
   return btree
 
 
@@ -31,3 +31,10 @@ def binaryTreeToTree(biTree):
 
 
 #实质：树的左边比较优先，能够成为老大，在儿子兄弟表示法中可以成为父亲
+
+
+#树的前序遍历序列和儿子兄弟树的前序遍历序列一致
+#树的后序遍历序列和儿子兄弟树的中序遍历序列一致
+
+#儿子兄弟树中，对应到树中，左手是儿子，右手是兄弟
+
