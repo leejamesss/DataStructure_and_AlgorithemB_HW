3b9bac6f39d01bb8e35b0e90025870ae9fcb20ec
james <117844938+leejamesss@users.noreply.github.com>
2023-04-04 16:08:19 +0800

    Update Forest.py

--- class/森林/Forest.py
+++ class/森林/Forest.py
@@ -1,12 +1,15 @@
 
 #森林转二叉树
 def woodToBinaryTree(woods):
-  biTree=woods[0]                                  #森林的根就是第一棵树的根
-  p=biTree                                         #指针，初始位置为森林的根
+  #woods是列表，每个元素都是一颗二叉树形式的树
+  biTree=woods[0]                                 
+  p=biTree                                         
   for i in range(1,len(woods)):                    
-    p.addRight(woods[i])                           #指针增加这个数目
+    p.addRight(woods[i])                           
     p=p.right
   return biTree
+#biTree和woods共用节点，直接在原地更改
+
 
 #二叉树转森林
 def binaryTreeToWoods(tree):
@@ -17,3 +20,5 @@ def binaryTreeToWoods(tree):
   if q:
     woods+=binaryTreeToWoods(q)
   return woods
+#woods是兄弟-儿子树的列表，woods和tree共用节点，执行完后tree的元素不再是原本的儿子兄弟树
+#就是没有复制节点，直接在原地更改
