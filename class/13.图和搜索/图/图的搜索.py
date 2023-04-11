698149e481700c98c5d2ea021937361c164463ec
james <117844938+leejamesss@users.noreply.github.com>
2023-04-11 17:44:46 +0800

    Update 图的搜索.py

--- class/13.图和搜索/图/图的搜索.py
+++ class/13.图和搜索/图/图的搜索.py
@@ -1,3 +1,32 @@
+#深搜
 def Dfs(V):
+  #标记V为旧点
+  #对和V相邻的每个点U;
+  #DFS(U)
+def main():
+  #将所有点标记为新点
+  #while(在图中能找到新点k)
+  #Dfs(k)
+
+
+  
+#ps.邻接表和邻接矩阵，判断两个点之间的关系，邻接表的时间O(1),邻接矩阵的时间O(2n)/O(n)
+  
+  
+  
+#图的深度优先遍历--邻接表形式
+def dfsTravel(G,op):
+  def dfs(v):
+    visited[v]=True
+    op(v)
+    for u in G[v]:
+      if not visited[u]:
+        dfs(u)
+  n=len(G)
+  visited=[False for i in range(n)]
+  for i in range(n):
+    if not visited[i]:
+      dfs(i)
+      
   
   
