2eb0649be4e54d3702c69b2436d901578e844c15
james <117844938+leejamesss@users.noreply.github.com>
2023-04-11 16:16:00 +0800

    Update 1.py

--- class/贪心算法/例题1/1.py
+++ class/贪心算法/例题1/1.py
@@ -5,4 +5,20 @@ class Candy:
     self.w=w
     
   def __lt__(self,other):
-    return
+    return (self.v/self.w-other.v/other.w)>eps
+n,w=list(map(int,input().split())
+candies=[Candy() for i in range(n)]
+for i in range(n):
+  candies[i].v,candies[i].w=list(map(float,input().split()))
+candies.sort()
+totalW=0
+totalV=0
+for i in range(n):
+  if (totalW + candies[i].w)<=w:
+    totalW += candies[i].w
+    totalV += cnadies[i].v
+  else:
+    totalV+= candies[i].v*float(w-totalW)/candies[i].w
+    break
+print('%.1f'%totalV)
+         
