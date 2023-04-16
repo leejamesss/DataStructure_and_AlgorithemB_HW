024973c19a8f1f1a4875319a7cb2694b4fd7e643
james <117844938+leejamesss@users.noreply.github.com>
2023-04-17 00:10:01 +0800

    Update 2.py

--- hw/hw08/2.py
+++ hw/hw08/2.py
@@ -1,17 +1,17 @@
-def findPostOrder(preorder):
-    if not preorder:
-        return []
-    root = preorder[0]
-    i = 1
-    while i < len(preorder):
-        if preorder[i] > root:
+def findPostOrder(preorder):     
+    if not preorder:            #如果preorder是空的，就返回空的list
+        return []           
+    root = preorder[0]          #根节点是preorder的第一个元素
+    i = 1                       #i是preorder中第一个大于root的元素的下标
+    while i < len(preorder):    #找到第一个大于root的元素
+        if preorder[i] > root:  
             break
-        i += 1
-    left = findPostOrder(preorder[1:i])
-    right = findPostOrder(preorder[i:])
-    return left + right + [root]
+        i += 1                  
+    left = findPostOrder(preorder[1:i])#递归地找左子树的后序遍历
+    right = findPostOrder(preorder[i:])#递归地找右子树的后序遍历
+    return left + right + [root]        #左子树的后序遍历+右子树的后序遍历+根节点
 
-n = int(input())
-preorder = list(map(int, input().split()))
-postorder = findPostOrder(preorder)
-print(' '.join(map(str, postorder)))
+n = int(input())                    
+preorder = list(map(int, input().split()))  
+postorder = findPostOrder(preorder) #找后序遍历
+print(' '.join(map(str, postorder)))    #输出后序遍历
